579cc2443fa81376dad8098eecfc1810e2f7b856
jjjwodls <jjjwodls@gmail.com>
2019-09-27 18:02:09 +0900

    문제 푸는중

--- StringTest/src/programmersAlgo/BiggestNumber.java
+++ StringTest/src/programmersAlgo/BiggestNumber.java
@@ -1,5 +1,11 @@
 package programmersAlgo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * 0 또는 양의 정수가 주어졌을 때, 정수를 이어 붙여 만들 수 있는 가장 큰 수를 알아내 주세요.
  * 
@@ -26,11 +32,75 @@ package programmersAlgo;
 public class BiggestNumber {
 
 	public static void main(String[] args) {
-
+		//int[] numbers = {6, 10, 2};
+		int[] numbers = {3, 30, 34, 5, 9}; 
+		
+		System.out.println(solution(numbers));
 	}
 	
-	public String solution(int[] numbers) {
+	public static String solution(int[] numbers) {
+		
         String answer = "";
+        List<Integer> descNumbersList = new ArrayList<Integer>();
+
+        int k = 0 ; 
+        String strNumber = "";
+     /*   for(int i = 0 ; i < numbers.length ; i++){
+        	k = numbers[i];
+        	strNumber += k; 
+        	for(int j = 0; j < numbers.length ; j++){
+        		if(k != numbers[j]){
+        			strNumber += numbers[j];
+        		}
+        	}
+        	descNumbersList.add( Integer.parseInt(strNumber) );
+        	strNumber = "" + k;
+        	for(int j = numbers.length-1; j >= 0 ; j--){
+        		if(k != numbers[j]){
+        			strNumber += numbers[j];
+        		}
+        	}
+        	descNumbersList.add( Integer.parseInt(strNumber) );
+        	strNumber = "";
+        }*/
+        
+        for(int i = 0 ; i < numbers.length ; i++){
+        	descNumbersList.add( numbers[i] );
+        }
+        
+        Collections.sort(descNumbersList, new Comparator<Integer>() {
+        	
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				String num1 = String.valueOf(o1);
+				String num2 = String.valueOf(o2);
+				int num1Len = num1.length() == 1 ? 1 :  num1.length()*10;
+				int num2Len = num2.length() == 1 ? 1 :  num2.length()*10;
+				
+				if(o1/num1Len > o2/num2Len){
+					return -1;
+				}else if(o1/num1Len > o2/num2Len){
+					return 1;
+				}else{
+					if(num1Len > num2Len){
+						return -1;
+					}else if(num1Len < num2Len){
+						return 1;
+					}else{
+						if(o1 % num1Len > o2%num2Len){
+							return -1;
+						}else if(o1 % num1Len < o2%num2Len){
+							return 1;
+						}else{
+							return 0;
+						}
+					}
+				} 
+			}
+		});
+        Integer biggestNum = descNumbersList.get(0);
+        answer = String.valueOf(biggestNum);
+        
         return answer;
     }
 
